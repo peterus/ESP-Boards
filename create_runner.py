d16bff04b34e29fc510a51373b8b13b8bc0d675f
Peter Buchegger <peter.buchegger7@gmail.com>
2023-07-19 21:45:06 +0000

    first test

---
+++ create_runner.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import os
+import json
+import docker
+from pathlib import Path
+
+SERIAL_BY_ID = Path("/dev/serial/by-id")
+
+usb_ids = os.listdir(SERIAL_BY_ID)
+
+f = open('boards.json')
+boards = json.load(f)
+
+found_boards = []
+for board in boards:
+    if board['usb id'] in usb_ids:
+        found_boards.append(board)
+
+for board in found_boards:
+    print(f"found board: {board['name']}")
+
+volumes=["locks:/locks", "/dev:/dev"]
+
+
+client = docker.from_env()
+for board in found_boards:
+    container = client.containers.list(filters={'name': board['name']})
+    if len(container) == 0:
+        print(f"starting {board['name']}")
+        environment=["USER=peterus", "REPOSITORY=ESP-Boards", f"LABELS={board['labels']}", f"RUNNER_NAME={board['name']}", f"ACCESS_TOKEN={os.getenv('ACCESS_TOKEN')}"]
+        container = client.containers.run("ghcr.io/peterus/esp-boards:main", detach=True, auto_remove=True, volumes=volumes, environment=environment, name=board['name'])
