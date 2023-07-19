f861986a065b4d235db281e647ea63d1a82ca58b
Peter Buchegger <peter.buchegger7@gmail.com>
2023-07-19 13:57:44 +0000

    add docker files

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USER=$USER
+REPOSITORY=$REPOSITORY
+ACCESS_TOKEN=$ACCESS_TOKEN
+LABELS=$LABELS
+RUNNER_NAME=$RUNNER_NAME
+
+curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${USER}/${REPOSITORY}/actions/runners/registration-token
+
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${USER}/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+
+echo $REG_TOKEN
+
+cd /home/docker/actions-runner
+
+./config.sh --url https://github.com/${USER}/${REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME} --labels ${LABELS} --ephemeral
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token ${REG_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
