8f6f6c17876257349c260c1d17b234057541cb80
Peter Buchegger <peter.buchegger7@gmail.com>
2023-07-25 19:15:14 +0000

    docker update

--- docker_image/start.sh
+++ docker_image/start.sh
@@ -5,20 +5,12 @@ REPOSITORY=$REPOSITORY
 ACCESS_TOKEN=$ACCESS_TOKEN
 LABELS=$LABELS
 RUNNER_NAME=$RUNNER_NAME
-USB_ID=$USB_ID
-
-curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${USER}/${REPOSITORY}/actions/runners/registration-token
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${USER}/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
-
-echo $REG_TOKEN
 
 cd /home/docker/actions-runner
 
 ./config.sh --url https://github.com/${USER}/${REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME} --labels ${LABELS}
-
-echo "USB_ID=$USB_ID" >> .env
-echo "RUNNER_NAME=$RUNNER_NAME" >> .env
 
 cleanup() {
     echo "Removing runner..."
